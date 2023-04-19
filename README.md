8c52d46e7af507af27061eae6b181c6a4b22ca42
Huderon <huderon.mcs@gmail.com>
2023-04-19 15:02:12 +1000

    init

---
+++ README.md
@@ -0,0 +1,3 @@
+# Discorb
+
+Reimagining of Discord's UI in a hand-drawn style, based on CHOLE's drawing.
---
+++ bd-scss.config.js
@@ -0,0 +1,10 @@
+/** @type {import('bd-scss/lib/config').Config} */
+export default {
+	meta: {
+		name: "Discorb",
+		author: "Huderon",
+		version: "0.0.1",
+		description: "Reimagining of Discord's UI in a hand-drawn style, based on CHOLE's drawing.",
+		source: "https://github.com/Huderon/Discorb"
+	}
+}
